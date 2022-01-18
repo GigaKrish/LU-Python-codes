c85594c8cdc6177e5e7867ed4fd4ab479741985b
GigaKrish <97963201+GigaKrish@users.noreply.github.com>
2022-01-18 22:56:36 +0530

    Add files via upload
    Let's Upgrade day 1 extra curricular practice work, submitted by Krish Sharma.

---
+++ day1  Krish.py
@@ -0,0 +1,10 @@
+given = int ( input ("Enter length of list (an integer) = ") )   # To define length of list
+lst = []                                    #an empty list to store values
+for i in range (given):     #for loop to run procedure desired times'''
+        i = (input("Desired list content: "))
+        lst.append(i)
+print()
+print("Your final list is:  " , lst)         #The desired output
+print()
+#Coded By Krish Sharma
+#email: ksysknp@gmail

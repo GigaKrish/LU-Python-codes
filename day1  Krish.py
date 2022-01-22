1b05598df455e9b543ccb8c4dcb6b34455ed5caa
GigaKrish <97963201+GigaKrish@users.noreply.github.com>
2022-01-22 17:11:05 +0530

    Update day1  Krish.py

--- day1  Krish.py
+++ day1  Krish.py
@@ -1,4 +1,5 @@
-given = int ( input ("Enter length of list (an integer) = ") )   # To define length of list
+print ("it is just a practice program based on Day 1 lecture")
+given = int ( input ("Enter length of list (an integer) = ") )   # To define length of list 
 lst = []                                    #an empty list to store values
 for i in range (given):     #for loop to run procedure desired times'''
         i = (input("Desired list content: "))
@@ -8,3 +9,4 @@ print("Your final list is:  " , lst)         #The desired output
 print()
 #Coded By Krish Sharma
 #email: ksysknp@gmail
+#I installed VS code and Python v.3.10 on Windows 10
